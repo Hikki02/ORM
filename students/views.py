6a3413ee9425abc4586abc7be69a6b2ae7f22aa8
Hikki02 <a.aidaraliev25@gmail.com>
2022-04-17 04:43:12 +0500

    add union

--- students/views.py
+++ students/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 from django.shortcuts import render
 
-from students.models import Student
+from students.models import Student, Teacher
 from django.db import connection
 
 
@@ -12,7 +12,7 @@ def student_list_(request):
     return render(request, 'output.html', {'posts': post})
 
 
-def student_list(request):
+def student_list__(request):
     post = Student.objects.filter(Q(last_name__startswith='Aidaraliev') |
                                   Q(last_name__startswith='last name 2') |
                                   Q(last_name__startswith='last name'))
@@ -21,3 +21,22 @@ def student_list(request):
     print(connection.queries)
 
     return render(request, 'output.html', {'posts': post})
+
+
+def student_list___(request):
+    post = Student.objects.filter(Q(class_room=4) & Q(first_name__startswith='Adilet'))
+
+    print(post)
+    print(connection.queries)
+
+    return render(request, 'output.html', {'posts': post})
+
+
+def student_list(request):
+    post = Student.objects.all().values_list('first_name').union(
+        Teacher.objects.all().values_list('first_name'))
+    print(post)
+    print(connection.queries)
+
+    return render(request, 'output.html', {'posts': post})
+
