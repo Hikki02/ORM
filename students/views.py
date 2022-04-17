b6d49a1da6dba47179eeda3c6ced51030faf5a50
Hikki02 <a.aidaraliev25@gmail.com>
2022-04-17 23:03:56 +0500

    output individual fields

--- students/views.py
+++ students/views.py
@@ -5,38 +5,61 @@ from students.models import Student, Teacher
 from django.db import connection
 
 
-def student_list_(request):
-    post = Student.objects.all()
-    print(post)
-    print(connection.queries)
-    return render(request, 'output.html', {'posts': post})
+# def student_list(request):
+#     post = Student.objects.all()
+#     print(post)
+#     print(connection.queries)
+#     return render(request, 'output.html', {'posts': post})
 
 
-def student_list__(request):
-    post = Student.objects.filter(Q(last_name__startswith='Aidaraliev') |
-                                  Q(last_name__startswith='last name 2') |
-                                  Q(last_name__startswith='last name'))
+# def student_list(request):
+#     post = Student.objects.filter(Q(last_name__startswith='Aidaraliev') |
+#                                   Q(last_name__startswith='last name 2') |
+#                                   Q(last_name__startswith='last name'))
+#
+#     print(post)
+#     print(connection.queries)
+#
+#     return render(request, 'output.html', {'posts': post})
 
-    print(post)
-    print(connection.queries)
 
-    return render(request, 'output.html', {'posts': post})
+# def student_list(request):
+#     post = Student.objects.filter(Q(class_room=4) & Q(first_name__startswith='Adilet'))
+#
+#     print(post)
+#     print(connection.queries)
+#
+#     return render(request, 'output.html', {'posts': post})
 
 
-def student_list___(request):
-    post = Student.objects.filter(Q(class_room=4) & Q(first_name__startswith='Adilet'))
+# def student_list(request):
+#     post = Student.objects.all().values_list('first_name').union(
+#         Teacher.objects.all().values_list('first_name'))
+#     print(post)
+#     print(connection.queries)
+#
+#     return render(request, 'output.html', {'posts': post})
 
-    print(post)
-    print(connection.queries)
 
-    return render(request, 'output.html', {'posts': post})
+# def student_list(request):
+#     post = Student.objects.exclude(age__gt=1)
+#
+#     print(post)
+#     print(connection.queries)
+#     return render(request, 'output.html', {'posts': post})
+
+
+# def student_list(request):
+#     post = Student.objects.filter(~Q(age=3) & ~Q(first_name__startswith='first name 1'))
+#
+#     print(post)
+#     print(connection.queries)
+#     return render(request, 'output.html', {'posts': post})
 
 
 def student_list(request):
-    post = Student.objects.all().values_list('first_name').union(
-        Teacher.objects.all().values_list('first_name'))
+    post = Student.objects.filter(Q(age=1)).only('first_name', 'class_room')
+
     print(post)
     print(connection.queries)
-
     return render(request, 'output.html', {'posts': post})
-
